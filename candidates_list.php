444ed4ce059d94341b49a6576b5aa5597c0c0bed
Fotie Joel <fotienwunjej@gmail.com>
2026-01-13 07:14:37 +0100

    Delete candidates_list.php

--- candidates_list.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../config.php';
-$candidates = $conn->query("SELECT * FROM Candidates");
-
-while ($c = $candidates->fetch_assoc()) {
-    echo $c['name'] . " (Event " . $c['event_id'] . ")<br>";
-}
-?>
